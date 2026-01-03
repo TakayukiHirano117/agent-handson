650930c432326f342ad537ce452184fe74f384f6
TakayukiHirano117 <ikusa.hirano@icloud.com>
2026-01-03 16:22:20 +0900

    2章が終わったお。おもろい

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # agent-handson
+
+➤ python3 -m venv .venv
+
+pip 25.3 from /Users/takayukihirano117/Desktop/dev/agent-handson/.venv/lib/python3.14/site-packages/pip (python 3.14)
+(.venv) takayukihirano117@guteisan:~/D/d/agent-handson|main⚡?
+➤ python -m pip install boto3==1.40.21 python-dotenv==1.1.1
---
+++ chapter2/1_converse.py
@@ -0,0 +1,23 @@
+# 必要なライブラリをインポート
+import boto3
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
+# Bedrock呼び出し用のAPIクライアントを作成
+client = boto3.client("bedrock-runtime")
+
+# Converse APIを実行
+response = client.converse(
+    modelId="us.anthropic.claude-3-7-sonnet-20250219-v1:0", # モデルID
+    messages=[{
+        "role": "user",
+        "content": [{
+            "text": "こんにちは" # 入力メッセージ
+        }]
+    }]
+)
+
+# 実行結果のテキストだけを画面に表示
+print(response["output"]["message"]["content"][0]["text"])
